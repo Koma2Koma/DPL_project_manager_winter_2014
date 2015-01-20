3651fb10a2bce0fae2454441f1e5185680c1c310
Carlos Garcia <carlos@devpointlabs.com>
2015-01-19 21:19:25 -0700

    Added callback action and User scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ActiveRecord::Base
 
   scope :with_github_state, ->(state_param) { where(github_state: state_param).where.not(github_state: nil) }
 
+  def self.from_omniauth(auth)
+    where("email = ? OR provider = ? AND uid = ? ", auth.info.email, auth.provider, auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
   def generate_github_state!
     self.update_attributes(github_state: self.generate_github_state)
     self.github_state
