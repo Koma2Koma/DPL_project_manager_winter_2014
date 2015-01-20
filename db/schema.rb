a3aa1b8cdb4ddc0e1c37c24683faf470121e30ed
Carlos Garcia <carlos@devpointlabs.com>
2015-01-19 20:57:05 -0700

    Add provider and uid to User model
    See https://github.com/plataformatec/devise/wiki/OmniAuth:-Overview

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119103654) do
+ActiveRecord::Schema.define(version: 20150120035533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20150119103654) do
     t.string   "github_state"
     t.string   "github_access_token"
     t.string   "github_username"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
